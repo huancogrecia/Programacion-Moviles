4732c40ebee4b0f30a0ae4ce6c29d8599790e5bf
Grecia Huanco <greciahuancob@gmail.com>
2026-08-26 00:16:28 -0500

    Agrega modelo de datos Producto

--- Semana-2/Sin-IA/Lab02CarritoKotlin/app/src/main/java/com/huanco/lab02carritokotlin/Carrito.kt
+++ Semana-2/Sin-IA/Lab02CarritoKotlin/app/src/main/java/com/huanco/lab02carritokotlin/Carrito.kt
@@ -1,5 +1,11 @@
 package com.huanco.lab02carritokotlin
 
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
 fun main() {
     println("=========================================")
     println(" CARRITO DE COMPRAS - TIENDA TECSUP ")

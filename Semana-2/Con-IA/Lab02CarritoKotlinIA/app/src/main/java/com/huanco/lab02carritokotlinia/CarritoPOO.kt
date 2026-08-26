a73283f4e784f1b6fe1c6adc133880ee155b6a1a
Grecia Huanco <greciahuancob@gmail.com>
2026-08-26 06:04:15 -0500

    Proyecto inicial del carrito con IA

---
+++ Semana-2/Con-IA/Lab02CarritoKotlinIA/app/src/main/java/com/huanco/lab02carritokotlinia/CarritoPOO.kt
@@ -0,0 +1,8 @@
+package com.huanco.lab02carritokotlinia
+fun main() {
+    println("=====CARRITO DE COMPRAS CON IA - TECSUP=====")
+
+    val nombreCliente = "Grecia Huanco"
+
+    println("Cliente: $nombreCliente")
+}

10c3ef9fbd353f81a313d3ac5904f17c545e563b
Grecia Huanco <greciahuancob@gmail.com>
2026-08-26 06:15:15 -0500

    Agrega clases de productos aplicando abstraccion y herencia

--- Semana-2/Con-IA/Lab02CarritoKotlinIA/app/src/main/java/com/huanco/lab02carritokotlinia/CarritoPOO.kt
+++ Semana-2/Con-IA/Lab02CarritoKotlinIA/app/src/main/java/com/huanco/lab02carritokotlinia/CarritoPOO.kt
@@ -1,4 +1,34 @@
 package com.huanco.lab02carritokotlinia
+
+abstract class Producto(
+    val nombre: String,
+    val precio: Double
+) {
+    abstract fun mostrarTipo(): String
+}
+
+
+class ProductoFisico(
+    nombre: String,
+    precio: Double
+) : Producto(nombre, precio) {
+
+    override fun mostrarTipo(): String {
+        return "Producto fisico"
+    }
+}
+
+class ProductoDigital(
+    nombre: String,
+    precio: Double
+) : Producto(nombre, precio) {
+
+    override fun mostrarTipo(): String {
+        return "Producto digital"
+    }
+}
+
+
 fun main() {
     println("=====CARRITO DE COMPRAS CON IA - TECSUP=====")
 

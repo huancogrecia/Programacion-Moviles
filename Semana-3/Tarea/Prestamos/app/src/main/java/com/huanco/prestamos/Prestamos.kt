12fd9ac668a1b86ec8eaf4bf08e32f730857e265
Grecia Huanco <greciahuancob@gmail.com>
2026-08-30 21:05:22 -0500

    Agrega calculo de atraso entre meses y años

--- Semana-3/Tarea/Prestamos/app/src/main/java/com/huanco/prestamos/Prestamos.kt
+++ Semana-3/Tarea/Prestamos/app/src/main/java/com/huanco/prestamos/Prestamos.kt
@@ -26,22 +26,58 @@ fun main() {
         else -> 0.00
     }
 
-    println("Ingrese la fecha de Prestamo:")
+    println()
+    println("Ingrese la fecha de Prestamo (dia/mes/año):")
     val fechaPrestamo = readln()
 
-    println("Ingrese la fecha de Entrega:")
+    println("Ingrese la fecha de Entrega (dia/mes/año):")
     val fechaEntrega = readln()
 
-    println("Ingrese fecha devolucion:")
+    println("Ingrese fecha devolucion (dia/mes/año):")
     val fechaDevolucion = readln()
 
-    val diaEntrega = fechaEntrega.substringBefore("/").toInt()
-    val diaDevolucion = fechaDevolucion.substringBefore("/").toInt()
+    val partesEntrega = fechaEntrega.split("/")
 
-    var diasAtraso = diaDevolucion - diaEntrega
+    var diaActual = partesEntrega[0].toInt()
+    var mesActual = partesEntrega[1].toInt()
+    var anioActual = partesEntrega[2].toInt()
 
-    if (diasAtraso < 0) {
-        diasAtraso = 0
+    val partesDevolucion = fechaDevolucion.split("/")
+
+    val diaDevolucion = partesDevolucion[0].toInt()
+    val mesDevolucion = partesDevolucion[1].toInt()
+    val anioDevolucion = partesDevolucion[2].toInt()
+
+    var diasAtraso = 0
+
+    while (
+        anioActual < anioDevolucion ||
+        (anioActual == anioDevolucion && mesActual < mesDevolucion) ||
+        (anioActual == anioDevolucion &&
+                mesActual == mesDevolucion &&
+                diaActual < diaDevolucion)
+    ) {
+
+        diaActual++
+
+        val diasMes = when (mesActual) {
+            1, 3, 5, 7, 8, 10, 12 -> 31
+            4, 6, 9, 11 -> 30
+            2 -> 28
+            else -> 0
+        }
+
+        if (diaActual > diasMes) {
+            diaActual = 1
+            mesActual++
+
+            if (mesActual > 12) {
+                mesActual = 1
+                anioActual++
+            }
+        }
+
+        diasAtraso++
     }
 
     val multaTotal = diasAtraso * multaPorDia
@@ -52,11 +88,12 @@ fun main() {
         "Devuelto a tiempo"
     }
 
-    println("SISTEMA DE DEVOLUCION DE LIBROS")
+    println()
+    println("+++SISTEMA DE DEVOLUCION DE LIBROS+++")
     println("Titulo del Libro: $titulo")
-    println("Tipo de Usuario:  $usuario")
+    println("Tipo de Usuario: $usuario")
     println("Fecha de Prestamo: $fechaPrestamo")
-    println("Fecha de Entrega:  $fechaEntrega")
+    println("Fecha de Entrega: $fechaEntrega")
     println("Fecha de Devolucion: $fechaDevolucion")
     println("Estado: $estado")
 
@@ -66,22 +103,43 @@ fun main() {
 
     var multaAcumulada = 0.0
 
-    val partesFecha = fechaEntrega.split("/")
-    val mes = partesFecha[1]
+    diaActual = partesEntrega[0].toInt()
+    mesActual = partesEntrega[1].toInt()
+    anioActual = partesEntrega[2].toInt()
 
     for (dia in 1..diasAtraso) {
 
+        diaActual++
+
+        val diasMes = when (mesActual) {
+            1, 3, 5, 7, 8, 10, 12 -> 31
+            4, 6, 9, 11 -> 30
+            2 -> 28
+            else -> 0
+        }
+
+        if (diaActual > diasMes) {
+            diaActual = 1
+            mesActual++
+
+            if (mesActual > 12) {
+                mesActual = 1
+                anioActual++
+            }
+        }
+
         multaAcumulada += multaPorDia
 
-        val fechaMulta = "${diaEntrega + dia}/$mes"
+        val fechaMulta = "$diaActual/$mesActual/$anioActual"
 
         println(
-            "$dia    | $fechaMulta     | S/ %.2f       | S/ %.2f".format(
+            "$dia    | $fechaMulta | S/ %.2f       | S/ %.2f".format(
                 multaPorDia,
                 multaAcumulada
             )
         )
     }
+
     println()
     println("+++DEUDA TOTAL+++")
     println("Total a pagar: S/ %.2f soles".format(multaTotal))

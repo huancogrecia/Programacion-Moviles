c3f048490276681adbf9a3e6b1ad62a562c5399e
Grecia Huanco <greciahuancob@gmail.com>
2026-09-03 21:58:47 -0500

    Agrega Slider con estado para los cursos

--- Semana-3/RegistroNotas-Tarea/app/src/main/java/com/huanco/registronotas/MainActivity.kt
+++ Semana-3/RegistroNotas-Tarea/app/src/main/java/com/huanco/registronotas/MainActivity.kt
@@ -5,14 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.huanco.registronotas.ui.theme.RegistroNotasTheme
@@ -36,6 +43,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PantallaRegistroNotas(modifier: Modifier = Modifier) {
+
+    var notaFundamentos by remember { mutableStateOf(0f) }
+    var notaPoo by remember { mutableStateOf(0f) }
+    var notaMoviles by remember { mutableStateOf(0f) }
+    var notaBaseDatos by remember { mutableStateOf(0f) }
 
     Column(
         modifier = modifier
@@ -62,18 +74,94 @@ fun PantallaRegistroNotas(modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        Text("Fundamentos de Programación (20%)")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Fundamentos de Programación (20%)",
+                modifier = Modifier.weight(1f)
+            )
+
+            Text(
+                text = notaFundamentos.toInt().toString()
+            )
+        }
+
+        Slider(
+            value = notaFundamentos,
+            onValueChange = { notaFundamentos = it },
+            valueRange = 0f..20f,
+            steps = 19,
+            modifier = Modifier.fillMaxWidth()
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text("Programación Orientada a Objetos (25%)")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Programación Orientada a Objetos (25%)",
+                modifier = Modifier.weight(1f)
+            )
+
+            Text(
+                text = notaPoo.toInt().toString()
+            )
+        }
+
+        Slider(
+            value = notaPoo,
+            onValueChange = { notaPoo = it },
+            valueRange = 0f..20f,
+            steps = 19,
+            modifier = Modifier.fillMaxWidth()
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text("Programación en Móviles (30%)")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Programación en Móviles (30%)",
+                modifier = Modifier.weight(1f)
+            )
+
+            Text(
+                text = notaMoviles.toInt().toString()
+            )
+        }
+
+        Slider(
+            value = notaMoviles,
+            onValueChange = { notaMoviles = it },
+            valueRange = 0f..20f,
+            steps = 19,
+            modifier = Modifier.fillMaxWidth()
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text("Base de Datos (25%)")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Base de Datos (25%)",
+                modifier = Modifier.weight(1f)
+            )
+
+            Text(
+                text = notaBaseDatos.toInt().toString()
+            )
+        }
+
+        Slider(
+            value = notaBaseDatos,
+            onValueChange = { notaBaseDatos = it },
+            valueRange = 0f..20f,
+            steps = 19,
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }

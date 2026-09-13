03e4b6327d311cf60fff19c47e8eaacf73434997
Grecia Huanco <greciahuancob@gmail.com>
2026-09-13 13:49:58 -0500

    Crear el modelo Tarea

--- Semana-4/Sin-IA/04-ListaTareas/app/src/main/java/com/huanco/listatareas/MainActivity.kt
+++ Semana-4/Sin-IA/04-ListaTareas/app/src/main/java/com/huanco/listatareas/MainActivity.kt
@@ -13,6 +13,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.huanco.listatareas.ui.theme.ListaTareasTheme
 
+data class Tarea(
+    val id: Int,
+    val nombre: String,
+    val completada: Boolean = false
+)
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

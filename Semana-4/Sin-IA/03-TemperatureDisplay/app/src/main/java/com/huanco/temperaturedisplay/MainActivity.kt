353e6f6fb1b075571728068c6878f82bd7058369
Grecia Huanco <greciahuancob@gmail.com>
2026-09-13 13:17:05 -0500

    Implementar controles para subir y bajar temperatura

--- Semana-4/Sin-IA/03-TemperatureDisplay/app/src/main/java/com/huanco/temperaturedisplay/MainActivity.kt
+++ Semana-4/Sin-IA/03-TemperatureDisplay/app/src/main/java/com/huanco/temperaturedisplay/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,10 +39,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TemperatureDisplay(modifier: Modifier = Modifier) {
     var temperatura by remember { mutableStateOf(20) }
-    Text(
-        text = "Temperatura: $temperatura °C",
-        modifier = modifier
-    )
+    Column(modifier = modifier) {
+        Text(
+            text = "Temperatura: $temperatura °C"
+        )
+        Row {
+            Button(
+                onClick = { temperatura++ }
+            ) {
+                Text("Subir")
+            }
+            Button(
+                onClick = { temperatura-- }
+            ) {
+                Text("Bajar")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)

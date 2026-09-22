ef311e7c5a0d474bbce49c1617804907105aa9fe
greciahuanco <greciahuancob@gmail.com>
2026-09-22 02:11:30 -0500

    Habilito la navegacion entre Inicio, Reservas, Rutinas y Perfil

--- Semana-5/Lab05_TECSUPfit_Huanco/app/src/main/java/com/huanco/tecsupfit_huanco/navegacion/NavegacionApp.kt
+++ Semana-5/Lab05_TECSUPfit_Huanco/app/src/main/java/com/huanco/tecsupfit_huanco/navegacion/NavegacionApp.kt
@@ -1,72 +1,106 @@
 package com.huanco.tecsupfit_huanco.navegacion
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.huanco.tecsupfit_huanco.componentes.BarraNavegacion
 import com.huanco.tecsupfit_huanco.pantallas.PantallaConfirmacion
 import com.huanco.tecsupfit_huanco.pantallas.PantallaDetalle
 import com.huanco.tecsupfit_huanco.pantallas.PantallaInicio
+import com.huanco.tecsupfit_huanco.pantallas.PantallaPerfil
 import com.huanco.tecsupfit_huanco.pantallas.PantallaReservas
+import com.huanco.tecsupfit_huanco.pantallas.PantallaRutinas
 
 @Composable
 fun NavegacionApp() {
 
     val navController = rememberNavController()
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    val rutaActual = backStackEntry.value?.destination?.route ?: Pantalla.Inicio.ruta
 
-    NavHost(
-        navController = navController,
-        startDestination = Pantalla.Inicio.ruta
-    ) {
-
-        composable(Pantalla.Inicio.ruta) {
-            PantallaInicio { claseId ->
-                navController.navigate(Pantalla.Detalle.crearRuta(claseId))
-            }
-        }
-
-        composable(
-            route = Pantalla.Detalle.ruta,
-            arguments = listOf(
-                navArgument("claseId") {
-                    type = NavType.IntType
+    Scaffold(
+        bottomBar = {
+            BarraNavegacion(
+                rutaActual = rutaActual,
+                onNavegar = { ruta ->
+                    navController.navigate(ruta)
                 }
             )
-        ) { backStackEntry ->
+        }
+    ) { paddingValues ->
 
-            val claseId = backStackEntry.arguments?.getInt("claseId") ?: 1
+        NavHost(
+            navController = navController,
+            startDestination = Pantalla.Inicio.ruta,
+            modifier = Modifier.padding(paddingValues)
+        ) {
 
-            PantallaDetalle(
-                claseId = claseId,
-                onReservar = {
+            composable(Pantalla.Inicio.ruta) {
+                PantallaInicio { claseId ->
                     navController.navigate(
-                        Pantalla.Confirmacion.crearRuta(claseId)
+                        Pantalla.Detalle.crearRuta(claseId)
                     )
                 }
-            )
-        }
-        composable(
-            route = Pantalla.Confirmacion.ruta,
-            arguments = listOf(
-                navArgument("claseId") {
-                    type = NavType.IntType
-                }
-            )
-        ) { backStackEntry ->
+            }
 
-            val claseId = backStackEntry.arguments?.getInt("claseId") ?: 1
+            composable(
+                route = Pantalla.Detalle.ruta,
+                arguments = listOf(
+                    navArgument("claseId") {
+                        type = NavType.IntType
+                    }
+                )
+            ) { backStackEntry ->
 
-            PantallaConfirmacion(
-                claseId = claseId,
-                onVerReservas = {
-                    navController.navigate(Pantalla.Reservas.ruta)
-                }
-            )
-        }
-        composable(Pantalla.Reservas.ruta) {
-            PantallaReservas()
+                val claseId = backStackEntry.arguments?.getInt("claseId") ?: 1
+
+                PantallaDetalle(
+                    claseId = claseId,
+                    onReservar = {
+                        navController.navigate(
+                            Pantalla.Confirmacion.crearRuta(claseId)
+                        )
+                    }
+                )
+            }
+
+            composable(
+                route = Pantalla.Confirmacion.ruta,
+                arguments = listOf(
+                    navArgument("claseId") {
+                        type = NavType.IntType
+                    }
+                )
+            ) { backStackEntry ->
+
+                val claseId = backStackEntry.arguments?.getInt("claseId") ?: 1
+
+                PantallaConfirmacion(
+                    claseId = claseId,
+                    onVerReservas = {
+                        navController.navigate(Pantalla.Reservas.ruta)
+                    }
+                )
+            }
+
+            composable(Pantalla.Reservas.ruta) {
+                PantallaReservas()
+            }
+
+            composable(Pantalla.Rutinas.ruta) {
+                PantallaRutinas()
+            }
+
+            composable(Pantalla.Perfil.ruta) {
+                PantallaPerfil()
+            }
         }
     }
 }

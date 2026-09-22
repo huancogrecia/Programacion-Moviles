21761f065747e113543a6fd7ab4a53cd0596b0f0
greciahuanco <greciahuancob@gmail.com>
2026-09-22 00:37:36 -0500

    Creo el proyecto base de TECSUP Fit

---
+++ Semana-5/Lab05_TECSUPfit_Huanco/app/src/main/java/com/huanco/tecsupfit_huanco/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.huanco.tecsupfit_huanco
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.huanco.tecsupfit_huanco.ui.theme.TECSUPfit_HuancoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TECSUPfit_HuancoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TECSUPfit_HuancoTheme {
+        Greeting("Android")
+    }
+}

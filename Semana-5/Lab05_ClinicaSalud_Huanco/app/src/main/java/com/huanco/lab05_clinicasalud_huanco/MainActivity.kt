56a46600483a135ac7e5e329df77c69653a61a98
greciahuanco <greciahuancob@gmail.com>
2026-09-22 08:32:59 -0500

    Creo el proyecto inicial de Clinica Salud

---
+++ Semana-5/Lab05_ClinicaSalud_Huanco/app/src/main/java/com/huanco/lab05_clinicasalud_huanco/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.huanco.lab05_clinicasalud_huanco
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.huanco.lab05_clinicasalud_huanco.ui.theme.Lab05_ClinicaSalud_HuancoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab05_ClinicaSalud_HuancoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab05_ClinicaSalud_HuancoTheme {
+        Greeting("Android")
+    }
+}

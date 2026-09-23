c58b619f888f600d8d253bc07191595750b51998
greciahuanco <greciahuancob@gmail.com>
2026-09-23 07:24:44 -0500

    Corrijo dependencias para sincronizar el proyecto

--- Semana-5/Lab05_Navegacion_Huanco/app/build.gradle.kts
+++ Semana-5/Lab05_Navegacion_Huanco/app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)

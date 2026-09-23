322ab6d14346ed1eeeaf2a9a11ba75a0ccf6f701
greciahuanco <greciahuancob@gmail.com>
2026-09-22 20:09:30 -0500

    Permito consultar el historial medico mostrando medico especialidad y fecha

--- Semana-5/Lab05_ClinicaSalud_Huanco/app/src/main/java/com/huanco/lab05_clinicasalud_huanco/pantallas/PantallaHistorialMedico.kt
+++ Semana-5/Lab05_ClinicaSalud_Huanco/app/src/main/java/com/huanco/lab05_clinicasalud_huanco/pantallas/PantallaHistorialMedico.kt
@@ -1,9 +1,46 @@
 package com.huanco.lab05_clinicasalud_huanco.pantallas
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PantallaHistorialMedico() {
-    Text(text = "Historial médico")
+
+    val historial = listOf(
+        Triple("Dr. Carlos Ruiz", "Cardiología", "10 de agosto"),
+        Triple("Dra. María López", "Medicina general", "22 de julio"),
+        Triple("Dr. José Pérez", "Pediatría", "15 de junio")
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+        Text(
+            text = "Historial médico",
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        LazyColumn {
+            items(historial) { consulta ->
+
+                Column(
+                    modifier = Modifier.padding(bottom = 16.dp)
+                ) {
+                    Text(text = "Médico: ${consulta.first}")
+                    Text(text = "Especialidad: ${consulta.second}")
+                    Text(text = "Fecha: ${consulta.third}")
+                }
+            }
+        }
+    }
 }
